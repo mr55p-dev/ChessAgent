4ec407fd50bf97f2b4b777fbb1a229df6ddbba5e
Ellis Lunnon <ellislunnon@gmail.com>
2022-05-07 12:14:16 +0100

    Fixed the engine changes

--- BadChess/stockfish.py
+++ BadChess/stockfish.py
@@ -1,37 +1,48 @@
 from subprocess import Popen, PIPE
 
-class Engine:
-    def __init__(self):
-        self._engine = Popen(["stockfish"], stdin=PIPE, stdout=PIPE)
+class Stockfish:
+    def __init__(self, movetime):
+        self._engine = Engine(movetime)
 
     def __enter__(self):
-        self._engine.stdin.write(b"ucinewgame\n")
-        self._engine.stdin.flush()
+        self._engine._flush_uci()
         return self._engine
 
     def __exit__(self, *args):
         self._engine.terminate()
 
 # Open an engine process and write the newgame command
+class Engine(Popen):
+    def __init__(self, movetime=1000) -> None:
+        super().__init__(["stockfish"], stdin=PIPE, stdout=PIPE)
+        self.stdin.write(b"uci\nisready\n")
+        self.stdin.flush()
+        self._movetime = movetime
 
+    def _flush_uci(self):
+        self.stdin.write(b"ucinewgame\n")
+        self.stdin.flush()
 
-def get_move(enigne_ctx, fen, movetime: int = 1000) -> str:
-    """Generator to write a new position to the engine. movetime is the duration to search for in ms"""
-    # Write the commands to the input
-    enigne_ctx.stdin.write(bytes(f"position {fen}\n", encoding="utf-8"))
-    enigne_ctx.stdin.write(bytes(f"go movetime {movetime}\n", encoding="utf-8"))
-    enigne_ctx.stdin.flush()
+    def set_state(self, move_hist):
+        hist_str = f"position startpos moves {' '.join(move_hist)}\n"
+        self.stdin.write(bytes(hist_str, encoding="utf-8"))
+        self.stdin.flush()
 
-    # Go over each output line and yield the bestmove output should
-    # it match the correct format
-    for line in enigne_ctx.stdout:
-        if len((split := str(line).strip().split(' '))) == 4:
-            return str(split[1])
-        else:
-            continue
-    raise ValueError("Stockfish failed to yield a best move.")
+    def get_move(self) -> str:
+        """Generator to write a new position to the engine. movetime is the duration to search for in ms"""
+        # Tell the engine we are in a new position
+        # self._flush_uci()
 
-with Engine() as engine_ctx:
-    move = get_move(engine_ctx, fen="rnb1kbnr/ppp1q1pp/8/3p3Q/3Pp3/8/PPP2PPP/RN2KBNR b KQkq - 1 5")
+        # Write the commands to the input
+        self.stdin.write(bytes(f"go movetime {self._movetime}\n", encoding="utf-8"))
+        self.stdin.flush()
 
-print(move)
+        # Go over each output line and yield the bestmove output should
+        # it match the correct format
+        for line in self.stdout:
+            # print(line)
+            if (split := line.decode().strip().split(' '))[0] == 'bestmove':
+                return str(split[1])
+            else:
+                continue
+        raise ValueError("Stockfish failed to yield a best move.")
--- badchess.py
+++ badchess.py
@@ -4,13 +4,16 @@ from pathlib import Path
 import chess
 from math import inf
 import tensorflow as tf
+import matplotlib.pyplot as plt
 
 from BadChess.generator import create_tfdata_set
 from BadChess.model import RNNGAN
 from BadChess.environment import search, Config
+from BadChess.stockfish import Stockfish
 
 
 def convert_and_save_model(model, path: Path) -> None:
+    """Save a model as a tflite file"""
     # Save the model
     saved_model_path = path.parent.joinpath("savedmodel")
     model.save_generator(saved_model_path)
@@ -22,15 +25,24 @@ def convert_and_save_model(model, path: Path) -> None:
         f.write(tflite_model)
 
 def run_train(args):
+    """Run model training"""
     gen_ds = create_tfdata_set(n_items=args.num_train, batch_size=args.batch, chunk_size=3, use_bitboard=True)
     dis_ds = create_tfdata_set(n_items=args.num_train, batch_size=args.batch, chunk_size=3, use_bitboard=False)
 
     model = RNNGAN()
-    model.train(
+    logs = model.train(
         args.epochs,
         gen_ds,
         dis_ds
     )
+    if args.graph:
+        # Save some graphs
+        for key in logs:
+            x_axis, y_axis = zip(*enumerate(logs[key]))
+            plt.plot(x_axis, y_axis)
+            plt.title(key)
+            plt.savefig(f'modelgraphs-{key}.png')
+            plt.clf()
 
     convert_and_save_model(model, Path("./models/generator_test_model.tflite"))
 
@@ -53,24 +65,31 @@ def run_game(args) -> None:
 
     # Setup some stuff
     board = chess.Board(args.start)
-    while not board.is_game_over():
-        print(f"Ply {board.ply()} - {'white' if board.turn else 'black'} to move")
-        if board.turn == chess.WHITE:
-            bestMove, withEval = search(board, args.engine_depth, True, -inf, inf, ())
-            print(bestMove)
-            print(f"Automated move: {bestMove} (evaluated at {withEval}) (searched {Config.num} positions).")
-            Config.reset_score()
+    with Stockfish(1000) as stockfish:
+        while not board.is_game_over():
+            print(f"Ply {board.ply()} - {'white' if board.turn else 'black'} to move")
+            if board.turn == chess.WHITE:
+                bestMove, withEval = search(board, args.engine_depth, True, -inf, inf, ())
+                print(bestMove)
+                print(f"Automated move: {bestMove} (evaluated at {withEval}) (searched {Config.num} positions).")
+                Config.reset_score()
 
-            board.push(bestMove)
-            print(board)
-        else:
-            move = input("Make a move: ")
-            try:
-                board.push_san(move)
+                board.push(bestMove)
                 print(board)
-            except ValueError:
-                print("Bad move")
-                continue
+            elif args.player:
+                move = input("Make a move: ")
+                try:
+                    board.push_san(move)
+                    print(board)
+                except ValueError:
+                    print("Bad move")
+                    continue
+            else:
+                move_list = [i.uci() for i in board.move_stack]
+                stockfish.set_state(move_list)
+                move = stockfish.get_move()
+                board.push(chess.Move.from_uci(move))
+                print(board)
 
     print(board.outcome())
 
@@ -82,12 +101,14 @@ training.add_argument("-e", "--epochs", help="Number of epochs to train for.", t
 training.add_argument("-b", "--batch", help="Batch size.", type=int, default=128)
 training.add_argument("-n", "--num_train", help="Number of examples to train on.", type=int, default=10_000)
 training.add_argument("-o", "--output", help="Output path to write a .tflite file to.", type=str, default="generator_model.tflite")
+training.add_argument("--graphs", help="Use graphs?", default=False, action="store_true", dest="graph")
 training.set_defaults(func=run_train)
 
 game = subparsers.add_parser("play")
 game.add_argument("model", help="Path to the model file")
 game.add_argument("-d", "--engine_depth", help="Search depth for the engine moves.", type=int, default=4)
 game.add_argument("-s", "--start", help="Starting position", type=str, default=chess.STARTING_FEN)
+game.add_argument("--player", help="Are you playing or is stockfish?", action="store_true", default=False)
 game.set_defaults(func=run_game)
 
 args = parser.parse_args()
